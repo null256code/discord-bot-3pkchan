53484e8af1d0f1c8983582a0f646207fca598b2b
null256code <19668528+null256code@users.noreply.github.com>
2022-04-09 15:24:08 +0900

    generateJooq導入

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jooq.meta.jaxb.ForcedType
+import org.jooq.meta.jaxb.Logging
+
 group = "nl2co"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
@@ -9,7 +12,16 @@ plugins {
     kotlin("plugin.spring") version Version.kotlin
 
     id("org.flywaydb.flyway") version Version.flyway
+    id("nu.studer.jooq") version "7.1.1"
 }
+
+//buildscript {
+//    configurations["classpath"].resolutionStrategy.eachDependency {
+//        if (requested.group == "org.jooq") {
+//            useVersion(Version.jooq)
+//        }
+//    }
+//}
 
 configurations {
     compileOnly {
@@ -31,12 +43,17 @@ dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-jooq")
 
     // -------------------------------------------------------------------
     implementation("com.discord4j:discord4j-core:3.1.3")
 
-    implementation("org.postgresql:postgresql:42.2.23")
+    implementation("org.postgresql:postgresql:${Version.postgres}")
     // implementation("org.flywaydb:flyway-core:${Version.flyway")
+
+    jooqGenerator("org.postgresql:postgresql:${Version.postgres}")
+    // https://github.com/etiennestuder/gradle-jooq-plugin/issues/209
+    jooqGenerator("jakarta.xml.bind:jakarta.xml.bind-api:3.0.1")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
@@ -61,4 +78,57 @@ flyway {
 //        "keyABC" to "valueXYZ",
 //        "otherplaceholder" to "value123"
 //    )
+}
+
+// https://github.com/etiennestuder/gradle-jooq-plugin#gradle-kotlin-dsl-4
+jooq {
+    edition.set(nu.studer.gradle.jooq.JooqEdition.OSS)  // default (can be omitted)
+
+    configurations {
+        create("main") {  // name of the jOOQ configuration
+            jooqConfiguration.apply {
+                logging = Logging.WARN
+                jdbc.apply {
+                    driver = "org.postgresql.Driver"
+                    url = "jdbc:postgresql://localhost:5432/postgres"
+                    user = "postgres"
+                    password = "postgres"
+//                    properties.add(Property().apply {
+//                        key = "ssl"
+//                        value = "true"
+//                    })
+                }
+                generator.apply {
+                    name = "org.jooq.codegen.DefaultGenerator"
+                    database.apply {
+                        name = "org.jooq.meta.postgres.PostgresDatabase"
+                        inputSchema = "public"
+                        forcedTypes.addAll(listOf(
+                            ForcedType().apply {
+                                name = "varchar"
+                                includeExpression = ".*"
+                                includeTypes = "JSONB?"
+                            },
+                            ForcedType().apply {
+                                name = "varchar"
+                                includeExpression = ".*"
+                                includeTypes = "INET"
+                            }
+                        ))
+                    }
+                    generate.apply {
+                        isDeprecated = false
+                        isRecords = true
+                        isImmutablePojos = true
+                        isFluentSetters = true
+                    }
+                    target.apply {
+                        packageName = "nu.studer.sample"
+                        directory = "build/generated-src/jooq/main"  // default (can be omitted)
+                    }
+                    strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+                }
+            }
+        }
+    }
 }
--- buildSrc/src/main/kotlin/Properties.kt
+++ buildSrc/src/main/kotlin/Properties.kt
@@ -1,5 +1,7 @@
 object Version {
     const val kotlin = "1.6.10"
-    const val flyway = "8.4.4"
     const val springBoot = "2.6.5"
+    const val flyway = "8.4.4"
+    const val jooq = " 3.16.5"
+    const val postgres = "42.2.23"
 }
