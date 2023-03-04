63add1c9a38bb71f37db00748a7eeec73d3308e0
null256code <19668528+null256code@users.noreply.github.com>
2023-03-05 02:38:37 +0900

    レシピのリンクをボタンにした

--- app/src/main/kotlin/spkchan/application/usecases/cooking/SearchRecipeUseCase.kt
+++ app/src/main/kotlin/spkchan/application/usecases/cooking/SearchRecipeUseCase.kt
@@ -1,6 +1,8 @@
 package spkchan.application.usecases.cooking
 
 import discord4j.core.event.domain.interaction.ApplicationCommandInteractionEvent
+import discord4j.core.`object`.component.ActionRow
+import discord4j.core.`object`.component.Button
 import discord4j.discordjson.json.ApplicationCommandRequest
 import discord4j.discordjson.json.ImmutableApplicationCommandRequest
 import org.springframework.stereotype.Component
@@ -34,14 +36,15 @@ class SearchRecipeUseCase(
             .build()
 
     override fun handle(event: ApplicationCommandInteractionEvent): Mono<*> {
-        val recipeRanking = fetchDailyMenuQuery.handle().recipes.joinToString("\n\n") {
+        val recipes = fetchDailyMenuQuery.handle().recipes
+        val recipeRanking = recipes.joinToString("\n\n") {
             """
 ${it.rank}位 ${it.title}
 【材料】${it.ingredientsDescription.replace(Regex("[\r\n]]"), " ")}
 【説明】${it.recipeDescription}
-${it.recipeUrl}
             """.trimIndent()
         }
+        val linkButtons = recipes.map { Button.link(it.recipeUrl, "${it.rank}位") }
 
         // 応答に3秒以上かかる場合はdeferReplyを使わないとタイムアウトする
         // https://docs.discord4j.com/interactions/application-commands#responding-to-commands
@@ -49,10 +52,11 @@ ${it.recipeUrl}
             event.createFollowup(
                 """
 今日の献立ランキングです
-
+```
 $recipeRanking
+```
                 """.trimIndent(),
-            ),
+            ).withComponents(ActionRow.of(linkButtons)),
         )
     }
 }
